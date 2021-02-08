58b880bdc91d2827600a1528307908e3903df74d
Rick Smoke <rsmoke@umich.edu>
2021-02-08 16:52:37 -0500

    added index and extra columns

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -15,5 +15,23 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  filter :email, as: :select
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column "Balance Due" do |u|
+      # number_to_currency(current_program.total_cost - u.payments.pluck(:total_amount).sum(&:to_f) / 100) 
+      number_to_currency(u.current_balance_due)
+    end
+    column :current_sign_in_at
+    column :sign_in_count
+    column :last_sign_in_at
+    column :last_sign_in_ip
+    column :created_at
+    actions
+  end
   
 end
