ff4817188f5725f363fc14910b4a5c34cad17139
Rick Smoke <rsmoke@umich.edu>
2021-02-10 18:23:26 -0500

    update for proper ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application,     'nelp_application'
 set :user,            'deployer'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :rbenv_ruby,      '2.7.1'
+set :rbenv_ruby,      '2.7.2'
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
